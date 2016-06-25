fc0cf949a316d93e79386bac1fd24dae32757d32
Christopher Millward <christopher.millward@gmail.com>
2016-06-24 20:52:41 -0400

    Added countdown timer.

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class CountdownTimer : MonoBehaviour {
+    public float duration = 60f;
+
+    Text m_countdownLabel;
+    float m_remaining;
+    bool m_hasElapsed = false;
+
+    void Awake() {
+        m_countdownLabel = GetComponent<Text> ();
+        if (m_countdownLabel == null) {
+            Debug.Log (string.Format("{0}: Countdown label isn't assigned", name));
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        ResetTimer ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!m_hasElapsed) {
+            int previousSeconds = Mathf.FloorToInt (m_remaining);
+            m_remaining -= Time.deltaTime;
+            int newSeconds = Mathf.FloorToInt (m_remaining);
+
+            if (previousSeconds != newSeconds && newSeconds >= 0) {
+                m_countdownLabel.text = newSeconds.ToString ();
+            }
+
+            if (m_remaining <= 0f) {
+                OnElapsed ();
+            }
+        }
+	}
+
+    void OnElapsed() {
+        m_hasElapsed = true;
+        MessageManager.Instance.SendMessage (new Message(this, "CountdownTimerElapsed", null));
+    }
+
+    void ResetTimer() {
+        m_remaining = duration;
+        m_hasElapsed = false;
+    }
+}
---
+++ Assets/Scripts/MessageManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Message {
+    public MonoBehaviour sender;
+    public string name;
+    public Dictionary<string, object> data;
+
+    public Message(MonoBehaviour sender, string name, Dictionary<string, object> data) {
+        this.sender = sender;
+        this.name = name;
+        this.data = data;
+    }
+}
+
+public delegate void MessageReceiver(Message message);
+
+public class MessageManager : MonoBehaviour {
+    Dictionary<string, List<MessageReceiver> > listeners = new Dictionary<string, List<MessageReceiver>>();
+
+    public static MessageManager Instance = null;
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+        else {
+            Destroy (gameObject);
+        }
+    }
+
+    public void AddListener(string messageName, MessageReceiver listener) {
+        if (!listeners.ContainsKey(messageName)) {
+            listeners[messageName] = new List<MessageReceiver>();
+        }
+        listeners[messageName].Add(listener);
+    }
+
+    public void SendMessage(Message message) {
+        if (listeners.ContainsKey(message.name)) {
+            List<MessageReceiver> recipients = listeners[message.name];
+            for (int i = 0; i < recipients.Count; ++i) {
+                recipients[i].Invoke(message);
+            }
+        }
+    }
+}
