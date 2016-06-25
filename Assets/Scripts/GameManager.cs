7247b2ed549e396c2bdba0cd9679c111d6985375
Christopher Millward <christopher.millward@gmail.com>
2016-06-24 22:05:01 -0400

    Loading but not doing anything with level data.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
 
 public class GameManager : MonoBehaviour {
     Player m_player;
+    public string levelResource;
 
 	// Use this for initialization
 	void Start () {
         MessageManager.Instance.AddListener ("CountdownTimerElapsed", OnCountdownTimerElapsed);
         MessageManager.Instance.AddListener ("ButtonPushed", OnButtonPushed);
         m_player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+        LoadLevel (levelResource);
 	}
 
     void OnCountdownTimerElapsed(Message message) {
@@ -18,5 +23,25 @@ public class GameManager : MonoBehaviour {
     void OnButtonPushed(Message message) {
         Debug.Log (string.Format("Button pushed: {0}", message.data["concept"]));
         m_player.Speak (message.data["concept"].ToString ());
+    }
+
+    void LoadLevel(string resourceName) {
+        TextAsset jsonAsset = Resources.Load<TextAsset>(resourceName);
+        if (jsonAsset != null) {
+            string fileContents = jsonAsset.text;
+            var N = JSON.Parse(fileContents);
+            var conceptArray = N["concepts"].AsArray;
+            foreach (JSONNode concept in conceptArray) {
+                string name = concept["name"];
+                string speech = concept["speech"];
+
+                Debug.Log (string.Format ("{0}: {1}", name, speech));
+
+                // @TODO Do something with this data.
+            }
+        }
+        else {
+            Debug.LogError("Unable to load level data from JSON at '" + resourceName + "': There was an error opening the file.");
+        }
     }
 }
