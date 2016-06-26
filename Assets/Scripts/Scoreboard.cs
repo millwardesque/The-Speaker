936b2435537de2a04e00f99d9373d93f74ddc1bb
Christopher Millward <christopher.millward@gmail.com>
2016-06-25 21:36:25 -0400

    Added coin sounds when the score goes up

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -4,6 +4,12 @@ using System.Collections;
 
 public class Scoreboard : MonoBehaviour {
     public Text scoreLabel;
+    public AudioClip[] soundClips;
+    AudioSource m_scoreSound;
+
+    void Awake() {
+        m_scoreSound = GetComponent<AudioSource> ();
+    }
 
     void Start() {
         MessageManager.Instance.AddListener ("ScoreUpdate", OnScoreUpdated);
@@ -12,5 +18,13 @@ public class Scoreboard : MonoBehaviour {
     void OnScoreUpdated(Message message) {
         int score = (int)message.data ["score"];
         scoreLabel.text = score.ToString ();
+
+        if (!m_scoreSound.isPlaying && score > 0) {
+            int index = Random.Range (0, soundClips.Length);
+            m_scoreSound.clip = soundClips [index];
+            m_scoreSound.pitch = Random.Range (0.7f, 1.3f);
+            m_scoreSound.volume = Random.Range (0.5f, 0.8f);
+            m_scoreSound.Play ();
+        }
     }
 }
